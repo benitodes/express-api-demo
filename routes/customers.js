b5243ab05ecaadc3074a465c3b2a0a10f6151962
benoit destresse <benoit@benoits-iMac.local>
2022-08-27 19:06:43 +0800

    correct validation error message on customer routes

--- routes/customers.js
+++ routes/customers.js
@@ -7,10 +7,18 @@ const customerSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minlength: 5,
+        maxlength: 50
     },
+    isGold: {
+        type: Boolean,
+        default: false
+        },
     phone: {
         type: String,
         required: true,
+        minlength: 5,
+        maxlength: 50
     }
 })
 
@@ -34,9 +42,13 @@ router.get('/:id', async (req, res) => {
 // POST customer Route
 
 router.post('/', async (req, res) => {
-    const result = validateCustomer(req.body);
-    if (result.error) return res.status(400).send(result.error.details[0].message);
-    let customer = new Customer({ name: req.body.name, phone: req.body.phone });
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let customer = new Customer({ 
+        name: req.body.name, 
+        phone: req.body.phone,
+        isGold: req.body.isGold
+    });
     customer = await customer.save();
     res.send(customer);
 })
@@ -45,8 +57,16 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     const { error } = validateCustomer(req.body);
-    if (error) return res.status(400).send(result.error.details[0].message);
-    const customer = await Customer.findByIdAndUpdate(req.params.id, { name: req.body.name, phone: req.body.phone }, {new: true });
+    if (error) return res.status(400).send(error.details[0].message);
+    let customer = await Customer.findByIdAndUpdate(req.params.id, { 
+        name: req.body.name, 
+        phone: req.body.phone,
+        isGold: req.body.isGold 
+    }, 
+    {
+        new: true
+    });
+
     if (!customer) return res.status(404).send("The customer with the given ID was not find");
     res.send(customer);
 })
@@ -63,8 +83,9 @@ router.delete('/:id', async (req, res) => {
 
 function validateCustomer(customer) {
     const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-        phone: Joi.string().min(8).required()
+        name: Joi.string().min(5).max(50).required(),
+        phone: Joi.string().min(5).max(50).required(),
+        isGold: Joi.boolean()
     });
 
     return schema.validate(customer);
--- routes/genres.js
+++ routes/genres.js
@@ -32,8 +32,8 @@ router.get('/:id', async (req, res) => {
 // POST genre route
 
 router.post('/', async (req, res) => {
-    const result = validateGenre(req.body);
-    if (result.error) return res.status(400).send(result.error.details[0].message);
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
     let genre = new Genre({ name: req.body.name});
     genre = await genre.save();
     res.send(genre);
@@ -44,7 +44,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     const { error } = validateGenre(req.body);
     if (error) return res.status(400).send(result.error.details[0].message);
-    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
+    let genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
     if (!genre) return res.status(404).send("The genre with the given ID was not find");
     res.send(genre);
 });
