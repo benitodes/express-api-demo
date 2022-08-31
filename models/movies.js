df8a1d1c44ac25786252e86a283879acd00f1a30
benoit destresse <benoit@benoit-destresse.local>
2022-08-31 13:17:23 +0800

    add movies model

---
+++ models/movies.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+})
+
+const Movie = new mongoose.model('Movie', movieSchema);
+
+// Validate with Joi
+function validateMovie(movie) {
+    const schema = Joi.object({
+        name: Joi.string()
+        .min(5)
+        .required()
+    });
+
+    return schema.validate(movie);
+};
+
+exports.Movie = Movie;
+exports.validate = validateMovie;
