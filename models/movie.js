aa6dadeb19edaba8c961c7ab247af27e859016b5
benoit destresse <benoit@benoits-iMac.local>
2022-09-05 07:37:02 +0800

    refactor validations and typos on movie models

--- models/movie.js
+++ models/movie.js
@@ -1,46 +1,43 @@
-const mongoose = require('mongoose');
 const Joi = require('joi');
-const { genreSchema } = require('./genre');
+const mongoose = require('mongoose');
+const {genreSchema} = require('./genre');
 
-const movieSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 5,
-        maxlength: 255
-    },
-    genre: { 
-        genreSchema,
-        required: true
-    },
-    numberInstock: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
-    },
-    dailyRentalRate: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 255
-    }
-})
+const Movie = mongoose.model('Movies', new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true, 
+    minlength: 5,
+    maxlength: 255
+  },
+  genre: { 
+    type: genreSchema,  
+    required: true
+  },
+  numberInStock: { 
+    type: Number, 
+    required: true,
+    min: 0,
+    max: 255
+  },
+  dailyRentalRate: { 
+    type: Number, 
+    required: true,
+    min: 0,
+    max: 255
+  }
+}));
 
-const Movie = new mongoose.model('Movie', movieSchema);
-
-// Validate with Joi
 function validateMovie(movie) {
-    const schema = Joi.object({
-        name: Joi.string().min(5).required(),
-        genreId: Joi.string().required(),
-        numberInstock: Joi.number().min(0).required(),
-        dailyRentalRate: Joi.number().min(0).required()
-    });
+  const schema = {
+    title: Joi.string().min(5).max(50).required(),
+    genreId: Joi.string().required(),
+    numberInStock: Joi.number().min(0).required(),
+    dailyRentalRate: Joi.number().min(0).required()
+  };
 
-    return schema.validate(movie);
-};
+  return schema.validate(movie);
+}
 
-exports.Movie = Movie;
+exports.Movie = Movie; 
 exports.validate = validateMovie;
