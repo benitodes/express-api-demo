e69a294a5efeaf9cadc39031fc7a875c768f85dd
Benoit Destresse <benoit.destresse@gmail.com>
2022-10-31 22:52:46 +0800

    added lodash

--- routes/users.js
+++ routes/users.js
@@ -19,11 +19,11 @@ router.post('/', async (req, res) => {
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User(_.pick(user, ['_id', 'name', 'email']));
+    user = new User(_.pick(req.body, ['_id', 'name', 'email', "password"]));
 
     await user.save();
 
-    res.send( _.pick(user, ['_id', 'name', 'email']));
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 })
 
 // Validate with Joi
