1e3b2d8a34742dddfe3a38d4ee59cceeab160dbb
Benoit Destresse <benoit.destresse@gmail.com>
2022-11-01 18:11:32 +0800

    added authentication to movies delete and post routes

--- routes/genres.js
+++ routes/genres.js
@@ -40,7 +40,7 @@ router.put('/:id', async (req, res) => {
 
 // DELETE genre route
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     const genre = await Genre.findByIdAndRemove(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not find');
     res.send(genre);
