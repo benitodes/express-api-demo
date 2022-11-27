a6e1b714f07bb69885a96f1fed27843ab65dac8e
Benoit Destresse <benoit.destresse@gmail.com>
2022-11-27 10:25:49 +0800

    refactor index.js by moving config functionto new module in startup folder

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-
-const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
 
 require('./startup/logging')();
+require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
 
-// Check env variable
-
-if (!config.get('jwtPrivateKey')) {
-    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-    process.exit(1);
-    }
-
-// app.set('view engine', 'pug');
 
 //PORT
 const port = process.env.PORT || 3000;
