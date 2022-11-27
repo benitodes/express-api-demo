f9304b4aedc52250b8fd33a9f4c64a5bf90c7c48
Benoit Destresse <benoit.destresse@gmail.com>
2022-11-27 10:28:55 +0800

    refactor index.js by moving validation to module in startup folder

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
+
 const express = require('express');
 const app = express();
 
@@ -7,8 +6,7 @@ require('./startup/logging')();
 require('./startup/config')();
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/validation')();
 
-
-//PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
