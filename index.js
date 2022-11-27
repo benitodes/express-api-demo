bede3e3a4cd6acd6407267a84d6a2f1986bba22a
Benoit Destresse <benoit.destresse@gmail.com>
2022-11-27 10:00:47 +0800

    refactor index.js by moving routes modules to startup file

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const home = require('./routes/home');
-const auth = require('./routes/auth');
-const debug = require ('debug')('app:startup');
-
+// const debug = require ('debug')('app:startup');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
 
 winston.handleExceptions(new winston.transports.File({ filename: 'unhandleExceptions.log'}))
 winston.add(new winston.transports.File({ filename: 'logfile.log' }));
 winston.add(new winston.transports.MongoDB({ db: 'mongodb://localhost:27017/video-app' }));
 
 // Connect to Mongodb
-const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/video-app', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'))
@@ -35,21 +29,6 @@ if (!config.get('jwtPrivateKey')) {
     }
 
 // app.set('view engine', 'pug');
-
-// Middleware
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
-app.use(auth);
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/users', users);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/auth', auth);
-app.use('/', home);
-app.use(error);
 
 //PORT
 const port = process.env.PORT || 3000;
